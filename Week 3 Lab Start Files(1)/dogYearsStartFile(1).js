8a0c096e9cbd8f2b983ac6e410422b9c6c5f4553
zennouni <152002106+zennouni@users.noreply.github.com>
2024-02-04 22:48:35 -0500

    Add files via upload

---
+++ Week 3 Lab Start Files(1)/dogYearsStartFile(1).js
@@ -0,0 +1,19 @@
+let myAge = 28;
+
+//store the value of 2
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+
+let laterYears = myAge - 2;
+//calc dog years
+laterYears *= 4;
+
+// Add earlyYears and laterYears together
+let myAgeInDogYears = earlyYears + laterYears;
+
+
+let myName = "zakaria".toLowerCase();
+
+// displays my name and age in dog years
+console.log(`My name is ${myName} and I am ${myAgeInDogYears} years old in dog years.`);
